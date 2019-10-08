76a4edfca7ec94cb7df670e9b53aac780da98f83
sfyc23 <sfyc23@gmail.com>
2019-10-08 12:08:38 +0800

    review

--- setup.py
+++ setup.py
@@ -7,11 +7,18 @@ Create time: 2019-10-06 00:33
 Introduction:
 """
 
-
 import io
 import os
-import re
-from setuptools import find_packages, setup, Command
+from setuptools import setup, Command
+import sys
+from shutil import rmtree
+
+NAME = 'wechat_notice'
+DESCRIPTION = 'Send Wechat a message at any time.'
+URL = 'https://github.com/sfyc23/wechat_notice'
+EMAIL = 'sfyc23@gmail.com'
+AUTHOR = 'DoubleThunder'
+VERSION = '0.0.03'
 
 
 # What packages are required for this module to be executed?
@@ -31,43 +38,72 @@ EXTRAS = {
 
 current_dir = os.path.abspath(os.path.dirname(__file__))
 
-def get_meta():
-    meta_re = re.compile(r"(?P<name>__\w+__) = '(?P<value>[^']+)'")
-    meta_d = {}
-    with open(os.path.join(current_dir, 'wechat_notice','__init__.py'),
-              encoding='utf8') as fp:
-        for match in meta_re.finditer(fp.read()):
-            meta_d[match.group('name')] = match.group('value')
-    return meta_d
-
 try:
     with io.open(os.path.join(current_dir, 'README.md'), encoding='utf-8') as f:
         long_description = '\n' + f.read()
 except FileNotFoundError:
-    long_description = '随时随地发消息到微信/工具.'
+    long_description = DESCRIPTION
 
 packages = [
     'wechat_notice',
 ]
 
-meta_d = get_meta()
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            # delete update cache.
+            rmtree(os.path.join(current_dir, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPi via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Publishing git tags…')
+        os.system('git tag v{0}'.format(VERSION))
+        os.system('git push --tags')
+
+        sys.exit()
+
+
+
 # Where the magic happens:
 setup(
-    name=meta_d['__title__'],
-    version=meta_d['__version__'],
-    description='发消息通知给微信/工具.',
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
     long_description=long_description,
     long_description_content_type='text/markdown',
-    author=meta_d['__author__'],
-    author_email='sfyc23@gmail.com',
-    license=meta_d['__license__'],
+    author=AUTHOR,
+    author_email=EMAIL,
+    license='MIT',
     python_requires='>=3.6.0',
-    url='https://github.com/sfyc23/wechat-notice',
-    # packages=find_packages(exclude=('tests',)),
-    packages=packages,
+    url=URL,
+
+    # packages=packages,
     # If your package is a single module, use this instead of 'packages':
     # py_modules=['mypackage'],
-
+    py_modules=['wechat_notice'],
     # entry_points={
     #     'console_scripts': ['mycli=mymodule:cli'],
     # },
@@ -86,7 +122,9 @@ setup(
         'Programming Language :: Python :: Implementation :: PyPy'
     ],
     keywords='wechat, 微信, notice, 通知',
-
+    # cmdclass={
+    #         'upload': UploadCommand,
+    # },
 )
 
 
